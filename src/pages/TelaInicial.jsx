4d4d86e67fefc4ae0e6111fac3bf9ad1445c6fd0
Nicolas Domingos <nicolasdomingoss264@gmail.com>
2025-10-10 16:18:26 -0300

    adicao dos botoes

--- src/pages/TelaInicial.jsx
+++ src/pages/TelaInicial.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import Calendar from '../components/Calendar'
+import Calendar from "../components/Calendar";
 import "./TelaInicial.css";
+
 const TelaInicial = () => {
   return (
-      <div className="main-content">
-        <div className="telaInicial-page">
-          <div className="cronograma-container">
-            <h1>CRONOGRAMA</h1>
-            </div>
-
-          <Calendar/>
+    <div className="main-content">
+      <div className="telaInicial-page">
+        <div className="cronograma-container">
+          <h1>CRONOGRAMA</h1>
         </div>
-        
-    </div>
 
+        <div className="calendar-area">
+          <Calendar />
+        </div>
+
+        <div className="botoes-container">
+          <button className="botao-criar">criar matéria</button>
+          <button className="botao-editar">editar</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
