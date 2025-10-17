46abadba23980a686f03fd85e5ade2b672aaec2f
Nicolas Domingos <nicolasdomingoss264@gmail.com>
2025-10-17 16:04:44 -0300

    modal criar materia

---
+++ src/components/ModalCriarMateria.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./ModalCriarMateria.css";
+
+const ModalCriarMateria = ({ onClose }) => {
+  return (
+    <div className="main-content">
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-button" onClick={onClose}>
+          ×
+        </button>
+        <h2>Criar Matéria</h2>
+
+        <form className="form-materia">
+          <label>Nome</label>
+          <input type="text" placeholder="adicione um nome para sua matéria" />
+
+          <label>descrição (opcional)</label>
+          <textarea placeholder="Fale sobre o conteúdo da sua matéria" />
+
+          <label>data de conclusão</label>
+          <div className="select-wrapper">
+            <select>
+              <option value="">selecionar</option>
+              <option value="1sem">1 semestre</option>
+              <option value="2sem">2 semestres</option>
+            </select>
+          </div>
+
+          <label>personalização</label>
+          <div className="color-row">
+            <input type="color" />
+            <div className="color-bar"></div>
+          </div>
+
+          <button type="submit" className="botao-confirmar">
+            confirmar
+          </button>
+        </form>
+      </div>
+    </div>
+</div>
+  );
+};
+
+export default ModalCriarMateria;
